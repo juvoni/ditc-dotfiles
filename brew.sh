3554bf44f6246891b424759e36be1b90fb67c111
Michael McDermott <michael@mgmcdermott.com>
2016-05-19 11:39:37 -0400

    Add brew cask applications.

--- brew.sh
+++ brew.sh
@@ -5,10 +5,26 @@ which -s brew
 if [[ $? != 0 ]] ; then
     # Install Homebrew
     # https://github.com/mxcl/homebrew/wiki/installation
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
     brew update
 fi
 
+brew install zsh
+brew install gnupg
+
 brew cask install atom
 brew cask install java
+brew cask install google-chrome
+brew cask install google-drive
+brew cask install alfred
+brew cask install dash
+brew cask install iterm2
+brew cask install virtualbox
+brew cask install caffeine
+brew cask install the-unarchiver
+brew cask install transmit
+brew cask install evernote
+
+brew cask cleanup
+brew cleanup
