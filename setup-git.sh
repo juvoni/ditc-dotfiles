96e75289a5c2a28597b2f5a4a9625e32e48afb3e
Michael McDermott <michael@mgmcdermott.com>
2016-05-23 10:16:43 -0400

    Add show object alias.

--- setup-git.sh
+++ setup-git.sh
@@ -11,7 +11,8 @@ git config --global alias.dp "diff --word-diff --unified=10"
 git config --global alias.qm "!git checkout $1;git merge @{-1}"
 ## Pretty history
 git config --global alias.lg "log --pretty='%Cred%h%Creset | %C(yellow)%d%Creset %s %Cgreen(%cr)%Creset %C(cyan)[%an]%Creset' --graph"
-
+## Show object
+git config --global alias.so "show --pretty='parent %Cred%p%Creset commit %Cred%h%Creset%C(yellow)%d%Creset%n%n%w(72,2,2)%s%n%n%w(72,0,0)%C(cyan)%an%Creset %Cgreen%ar%Creset'"
 # Core settings
 ## Only paginate info like git diff if needed
 git config --global core.pager 'less -RFX'
